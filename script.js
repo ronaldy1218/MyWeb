b9ca22b25ca2ca8c9f70f0a32c4646d2191939ef
ronaldy1218 <ronaldy1218@yahoo.com>
2022-11-16 21:46:29 -0700

    CtoF

--- script.js
+++ script.js
@@ -24,12 +24,26 @@ async function getWeather(city) {
 function addWeather(data) {
 	const temp_c = Math.floor(data.current.temp_c)
 	const temp_f = Math.floor(data.current.temp_f)
-	main.innerHTML = `${temp_c}°C or ${temp_f}°F`;
+	main.innerHTML = `${temp_c}°C`;
 	const imagesrc = data.current.condition.icon
 	const condition = data.current.condition.text
 	p.innerHTML = `<img src="${imagesrc}"> ${condition} <img src="${imagesrc}">`
-}
 
+	const button = document.createElement("button");
+
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		if (main.innerHTML.includes("C")) {
+			main.innerHTML = `${temp_f}°F`
+		}
+		else  {
+		main.innerHTML = `${temp_c}°C`
+		}
+	});
+
+	main.insertAdjacentElement("afterend", button)	
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -40,5 +54,3 @@ form.addEventListener("submit", (e) => {
         getWeather(city);
     }
 });
-
-
