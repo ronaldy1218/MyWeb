77acb972a1f18e08a7d1db89e109daa6c7e4f9d4
ronaldy1218 <ronaldy1218@yahoo.com>
2022-11-16 20:24:19 -0700

    js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const url = (city) => `https://api.weatherapi.com/v1/current.json?key=${API_Key}
 const form = document.getElementById("form")
 const search = document.getElementById("search")
 const main = document.getElementById("main")
+const p = document.getElementById("p")
+
 
 async function getWeather(city) {
 	try{
@@ -20,10 +22,14 @@ async function getWeather(city) {
 
 }	
 function addWeather(data) {
-	const temp_c = data.current.temp_c
-	const temp_f = data.current.temp_f
-	main.innerHTML = `${temp_c} and ${temp_f}`;
+	const temp_c = Math.floor(data.current.temp_c)
+	const temp_f = Math.floor(data.current.temp_f)
+	main.innerHTML = `${temp_c}°C or ${temp_f}°F`;
+	const imagesrc = data.current.condition.icon
+	const condition = data.current.condition.text
+	p.innerHTML = `<img src="${imagesrc}"> ${condition} <img src="${imagesrc}">`
 }
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
